7e98cee06ad5ff383e8a7bbe63b8abd318a3b1e0
Erwin Kloosterboer <erwin@nfnty.nl>
2014-12-31 12:34:16 +0100

    User resource as example

--- apps/modules/api/resources/User.php
+++ apps/modules/api/resources/User.php
@@ -4,18 +4,90 @@ namespace App\Modules\Api\Resources;
 class User extends AbstractResource
 {
 
+    /**
+     * Create a resource
+     */
     public function create($data)
     {
-        // TODO: Implement create() method.
+        return array("method"=>"create");
     }
 
+    /**
+     * Delete a resource
+     *
+     * @param  mixed $id
+     * @return array|void
+     */
+    public function delete($id)
+    {
+        return array("method"=>"delete");
+    }
+
+    /**
+     * Delete a collection, or members of a collection
+     *
+     * @param  mixed $data
+     * @return array|void
+     */
+    public function deleteList($data)
+    {
+        return array("method"=>"deleteList");
+    }
+
+    /**
+     * Fetch a resource
+     *
+     * @param  mixed $id
+     * @return array
+     */
     public function fetch($id)
     {
-        return array("Fetching resource with id $id");
+        return array("method"=>"fetch");
     }
 
-    public function fetchAll()
+    /**
+     * Fetch all or a subset of resources
+     *
+     * @param  array $params
+     * @return array|void
+     */
+    public function fetchAll($params = array())
     {
-        return array("Fetching all available resources");
+        return array("method"=>"fetchAll");
+    }
+
+    /**
+     * Patch (partial in-place update) a resource
+     *
+     * @param  mixed $id
+     * @param  mixed $data
+     * @return array|void
+     */
+    public function patch($id, $data)
+    {
+        return array("method"=>"patch");
+    }
+
+    /**
+     * Replace a collection or members of a collection
+     *
+     * @param  mixed $data
+     * @return array|void
+     */
+    public function replaceList($data)
+    {
+        return array("method"=>"replaceList");
+    }
+
+    /**
+     * Update a resource
+     *
+     * @param  mixed $id
+     * @param  mixed $data
+     * @return array|void
+     */
+    public function update($id, $data)
+    {
+        return array("method"=>"update");
     }
 }
