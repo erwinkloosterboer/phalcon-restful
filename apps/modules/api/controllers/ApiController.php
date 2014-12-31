11e30fff409c113a8aca6afb52eb0d266220144c
Erwin Kloosterboer <erwin@nfnty.nl>
2014-12-31 12:33:57 +0100

    ApiController calls the right functions based on the combination of http method and id

--- apps/modules/api/controllers/ApiController.php
+++ apps/modules/api/controllers/ApiController.php
@@ -12,37 +12,54 @@ class ApiController extends Controller
     /**
      * dispatch rest
      * */
-    public function indexAction($resource = null, $id=null)
+    public function indexAction($resource = null, $id = null)
     {
         $method = $this->request->getMethod();
 
-        $resource = ucfirst($resource);
+        $resource = ucfirst(strtolower($resource));
         $className = "App\\Modules\\Api\\Resources\\$resource";
 
-        if(!class_exists($className)){
+        if (!class_exists($className)) {
             $return = array("resource not found");
-        }else{
-            /* @var $class AbstractResource */
+        } else {
+
+            //The class exists, create it
             $class = new $className();
 
-            switch ($method) {
-                case 'GET':
-                    if (!empty($id)) {
-                        $return = $class->fetch($id);
-                    } else {
-                        $return = $class->fetchAll();
-                    }
-                    break;
-                case 'POST':
-                    $response = Services::getService($this->_model)->save($input);
-                    break;
-                case 'PUT':
-                    $response = Services::getService($this->_model)->update($input, $id);
-                    break;
-                case 'DELETE':
-                    $response = Services::getService($this->_model)->delete($id);
-                    break;
+            if (!($class instanceof AbstractResource)) {
+                $return = array("Resource not an instance of AbstractResource");
+            } else {
+                switch ($method) {
+                    case 'POST':
+                        $return = $class->create($this->request->getPost());
+                        break;
+                    case 'DELETE':
+                        if ($id !== null) {
+                            $return = $class->delete($id);
+                        } else {
+                            $return = $class->deleteList($this->request->getPost());
+                        }
+                        break;
+                    case 'GET':
+                        if ($id !== null) {
+                            $return = $class->fetch($id);
+                        } else {
+                            $return = $class->fetchAll();
+                        }
+                        break;
+                    case 'PATCH':
+                        $return = $class->patch($id, $this->request->getPost());
+                        break;
+                    case 'PUT':
+                        if ($id !== null) {
+                            $return = $class->update($id, $this->request->getPost());
+                        } else {
+                            $return = $class->replaceList($this->request->getPost());
+                        }
+                        break;
+                }
             }
+
         }
 
         header("Content-Type: Application/json");
